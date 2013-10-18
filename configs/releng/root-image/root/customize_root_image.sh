3fcbea1575f5fe9f5f1dc57674279ba1cc229235
NgoHuy <huynhok.uit@gmail.com>
2013-10-18 20:23:54 +0700

    Change as follow + Change shell to bash as default + LXDM as default display manager
    + Login to guest instead of root
    
    + Sudo without password

--- configs/releng/root-image/root/customize_root_image.sh
+++ configs/releng/root-image/root/customize_root_image.sh
@@ -7,15 +7,23 @@ locale-gen
 
 ln -sf /usr/share/zoneinfo/UTC /etc/localtime
 
-usermod -s /usr/bin/zsh root
-cp -aT /etc/skel/ /root/
+# usermod -s /usr/bin/bash root
+# cp -aT /etc/skel/ /root/
 
-useradd -m -p "" -g users -G "adm,audio,floppy,log,network,rfkill,scanner,storage,optical,power,wheel" -s /usr/bin/zsh arch
+# check if user exists
+user=$(grep guest /etc/passwd | cut -f1 -d :)
+if [ "$user" = "" ]; then
+	useradd -m -p "" -g users -G "adm,audio,floppy,log,network,rfkill,scanner,storage,optical,power,wheel" -s /bin/bash guest
+	sed -i 's|# autologin=.*|autologin=guest|' /etc/lxdm/lxdm.conf
+	sed -i 's|# session=/usr/bin/startlxde|session=/usr/bin/enlightenment_start|' /etc/lxdm/lxdm.conf
+	sed -i 's|# %wheel .*|%wheel ALL=(ALL) NOPASSWD: ALL|' /etc/sudoers
+fi
 
-chmod 750 /etc/sudoers.d
-chmod 440 /etc/sudoers.d/g_wheel
+ #chmod 750 /etc/sudoers.d
+ #chmod 440 /etc/sudoers.d/g_wheel
 
 sed -i "s/#Server/Server/g" /etc/pacman.d/mirrorlist
 sed -i 's/#\(Storage=\)auto/\1volatile/' /etc/systemd/journald.conf
 
-systemctl enable multi-user.target pacman-init.service choose-mirror.service
+
+systemctl enable pacman-init.service choose-mirror.service lxdm.service
